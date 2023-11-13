1c4a7d14812f755c2f5e018d6c28ebe10453230c
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-11-13 16:11:46 -0300

    feat: add initial header component

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-gradient-cvlb h-20">
+      <div className="container relative h-full">
+        <div className="absolute left-1/2 flex h-24 w-60 -translate-x-1/2 items-center bg-background shadow-xl">
+          <Link href="/" className="w-full">
+            <Image
+              src="/cvlb-logo.svg"
+              alt="Imagem do logo CVLB"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-16 w-full object-contain"
+              quality={100}
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMMvAYAAX4BKd5Md4EAAAAASUVORK5CYII="
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
