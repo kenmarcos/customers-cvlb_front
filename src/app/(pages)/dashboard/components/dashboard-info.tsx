393fcd50f3664a8c77810176721e7c3c57842e62
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-11-14 09:17:41 -0300

    feat: add DashboardInfo component

---
+++ src/app/(pages)/dashboard/components/dashboard-info.tsx
@@ -0,0 +1,14 @@
+const DashboardInfo = () => {
+  return (
+    <section>
+      <h1 className="mb-2 text-4xl font-bold">O que deseja fazer?</h1>
+
+      <p className="text-muted-foreground">Cadastre novos clientes.</p>
+      <p className="text-muted-foreground">
+        Tenha acesso às informações de todos os clientes.
+      </p>
+    </section>
+  );
+};
+
+export default DashboardInfo;
